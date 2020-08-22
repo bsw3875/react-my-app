74947de2618d533d49fb763c975f39fa29c2c36f
barbara_workman <susan.workman90@gmail.com>
2020-08-22 16:32:21 -0400

    Updates

---
+++ precache-manifest.655b6fbb9a5a256a1a54068cf82c9983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d18aafa34b97ebec167486a87a1f875",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "257f073cc12086e1fbbd",
+    "url": "/my-app/static/css/main.de434962.chunk.css"
+  },
+  {
+    "revision": "b5910899a923e67feffb",
+    "url": "/my-app/static/js/2.24506f84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.24506f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257f073cc12086e1fbbd",
+    "url": "/my-app/static/js/main.cd8351d9.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
